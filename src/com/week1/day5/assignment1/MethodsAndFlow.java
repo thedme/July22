a7c39c006401a0d6b17752ed9f94933655e96067
Dovid Eskenazi <dvdsknz@gmail.com>
2016-07-22 16:24:15 -0400

    07/22/2016

--- src/com/week1/day5/assignment1/MethodsAndFlow.java
+++ src/com/week1/day5/assignment1/MethodsAndFlow.java
@@ -1,35 +1,49 @@
+
+/*
+ * I received a lot of assistance from fellow boot-campers for this optional assignment
+ * 
+ * 
+ * 
+ */
+
+
+
 package com.week1.day5.assignment1;
 
 public class MethodsAndFlow
 {
-    //Data members
-    int num;
-    String words = "";
-    
-    String[] stringArray = {"This is cool"};
-    
-    //Constructor
-    
-    public MethodsAndFlow()
-    {
-        
-    }
-    
-    public MethodsAndFlow(int num)
-    {
-       this(); 
-       this.num = num;
-    }
-    
-    public MethodsAndFlow(String words)
-    {
-       this(5); 
-       this.words = words;
-    
-}
-  
+    // Data member
+
+    private String[] stringArray;
+
+    // Constructor
+
     public MethodsAndFlow(String[] stringArray)
     {
-        
-}
+        this.stringArray = stringArray;
+    }
+
+    // methods
+
+    public void outputSubset(int startPosition, int numberOfValues)
+    {
+
+        if (startPosition > 0 && startPosition < stringArray.length)
+        {
+            for (int i = startPosition; i < (startPosition + numberOfValues); i++)
+            {
+                System.out.println(stringArray[i]);
+            }
+
+        } else
+        {
+            System.out.println("Error");
+        }
+    }
+
+    public void outputAll()
+    {
+        outputSubset(0, stringArray.length);
+    }
+
 }
---
+++ src/com/week1/day5/assignment1/TestMethodsandFlow.java
@@ -0,0 +1,16 @@
+package com.week1.day5.assignment1;
+
+public class TestMethodsandFlow
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        MethodsAndFlow duke = new MethodsAndFlow(args);
+        
+        duke.outputAll();
+        duke.outputSubset(0, 0);
+
+    }
+
+}
